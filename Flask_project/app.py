471a39133ab431a5b517edef74e48f7b3471a172
Meghana7333 <110120723+Meghana7333@users.noreply.github.com>
2024-03-06 14:11:23 +0530

    feat:added disease

--- Flask_project/app.py
+++ Flask_project/app.py
@@ -1,8 +1,87 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,Markup
 import pickle
 import numpy as np
 import pandas as pd
+from utils.disease import disease_dic
+import requests
+import io
+import torch
+from torchvision import transforms
+from PIL import Image
+from utils.model import ResNet9
 import sklearn
+from flask import Flask, render_template, request, redirect, url_for
+
+#--
+# Loading plant disease classification model
+
+disease_classes = ['Apple___Apple_scab',
+                   'Apple___Black_rot',
+                   'Apple___Cedar_apple_rust',
+                   'Apple___healthy',
+                   'Blueberry___healthy',
+                   'Cherry_(including_sour)___Powdery_mildew',
+                   'Cherry_(including_sour)___healthy',
+                   'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
+                   'Corn_(maize)___Common_rust_',
+                   'Corn_(maize)___Northern_Leaf_Blight',
+                   'Corn_(maize)___healthy',
+                   'Grape___Black_rot',
+                   'Grape___Esca_(Black_Measles)',
+                   'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
+                   'Grape___healthy',
+                   'Orange___Haunglongbing_(Citrus_greening)',
+                   'Peach___Bacterial_spot',
+                   'Peach___healthy',
+                   'Pepper,_bell___Bacterial_spot',
+                   'Pepper,_bell___healthy',
+                   'Potato___Early_blight',
+                   'Potato___Late_blight',
+                   'Potato___healthy',
+                   'Raspberry___healthy',
+                   'Soybean___healthy',
+                   'Squash___Powdery_mildew',
+                   'Strawberry___Leaf_scorch',
+                   'Strawberry___healthy',
+                   'Tomato___Bacterial_spot',
+                   'Tomato___Early_blight',
+                   'Tomato___Late_blight',
+                   'Tomato___Leaf_Mold',
+                   'Tomato___Septoria_leaf_spot',
+                   'Tomato___Spider_mites Two-spotted_spider_mite',
+                   'Tomato___Target_Spot',
+                   'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+                   'Tomato___Tomato_mosaic_virus',
+                   'Tomato___healthy']
+
+disease_model_path = 'models/plant-disease-model.pth'
+disease_model = ResNet9(3, len(disease_classes))
+disease_model.load_state_dict(torch.load(
+    disease_model_path, map_location=torch.device('cpu')))
+disease_model.eval()
+
+def predict_image(img, model=disease_model):
+    """
+    Transforms image to tensor and predicts disease label
+    :params: image
+    :return: prediction (string)
+    """
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.ToTensor(),
+    ])
+    image = Image.open(io.BytesIO(img))
+    img_t = transform(image)
+    img_u = torch.unsqueeze(img_t, 0)
+
+    # Get predictions from model
+    yb = model(img_u)
+    # Pick index with highest probability
+    _, preds = torch.max(yb, dim=1)
+    prediction = disease_classes[preds[0].item()]
+    # Retrieve the class label
+    return prediction
+#--
 crop_recommendation_model_path ='models/model.pkl' 
 crop_recommendation_model = pickle.load(
     open(crop_recommendation_model_path, 'rb'))
@@ -34,7 +113,7 @@ def Weed_detect():
 
 @ app.route('/Yield')
 def yield_prediction():
-    title = 'Harvestify - Yield Prediction'
+    title = 'Agro Check - Yield Prediction'
 
     return render_template('yield.html', title=title)
 
@@ -77,7 +156,7 @@ def crop_prediction():
 
 @ app.route('/yield-predict', methods=['POST'])
 def yield_predict():
-    title = 'Harvestify - Yield Prediction'
+    title = 'Agro Check - Yield Prediction'
     crop_name=str(request.form['cropname'])
     state_name=str(request.form['statename'])
     season_name=str(request.form['seasonname'])
@@ -99,7 +178,7 @@ def yield_predict():
     user_input=[area,Production,Annual_rainfall,Fertilizer,Pesticide,crop_name,state_name,season_name]
     user_input_df = pd.DataFrame([user_input])
     predicted_yield = yield_prediction_model.predict(user_input_df)
-    result=f"Predicted yield production: {predicted_yield[0][0]}"
+    result=f"Predicted yield production: {predicted_yield[0][0]} per unit area"
     if(predicted_yield[0][0]<0):
         result="Sorry we are unable to predict"
 
@@ -111,15 +190,92 @@ def yield_predict():
 @app.route('/disease-predict', methods=['GET', 'POST'])
 def disease_prediction():
     title = 'Agro Check - Disease Detection'
+
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files.get('file')
+        if not file:
+            return render_template('disease.html', title=title)
+        try:
+            img = file.read()
+
+            prediction = predict_image(img)
+
+            prediction = Markup(str(disease_dic[prediction]))
+            return render_template('disease-result.html', prediction=prediction, title=title)
+        except:
+            pass
     return render_template('disease.html', title=title)
 
 @app.route('/weed-predict', methods=['GET', 'POST'])
 def weed_detection():
     title = 'Agro Check - Weed Detection'
     return render_template('weed.html', title=title)
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file part'
+    
+    file = request.files['file']
+    
+    if file.filename == '':
+        return 'No selected file'
 
+    # Ensure the 'uploads' directory exists
+    upload_folder = os.path.join(os.getcwd(), 'static/uploads')
+    os.makedirs(upload_folder, exist_ok=True)
 
+    # Save the file to the 'uploads' folder
+    file.save(os.path.join(upload_folder, file.filename))
 
+    # Redirect to a page that displays the uploaded image
+    return redirect(url_for('display_image', filename=file.filename))
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.models import load_model
+
+# Load the model
+import os
+modelres = load_model("resnet.h5")
+@app.route('/display/<filename>')
+def display_image(filename):
+    image_path = os.path.join(os.getcwd(), 'static/uploads/'+filename)
+
+    # Check if the file exists
+    if not os.path.exists(image_path):
+        return 'Image not found'
+
+    # Read the image using OpenCV
+    image = cv2.imread(image_path)
+
+    # Resize the image to match the input size expected by the model
+    shape = 224  # Assuming the model expects input images of size 224x224
+    image_resized = cv2.resize(image, (shape, shape))
+
+    # Reshape the image to match the input shape expected by the model
+    image_reshaped = np.reshape(image_resized, (1, shape, shape, 3))
+
+    # Make predictions using the loaded model
+    pred = modelres.predict(image_reshaped)
+
+    # Extract bounding box predictions
+    bbox_predictions = pred[1][0]
+
+    # Get coordinates of the bounding box
+    startX = int(bbox_predictions[0] * shape)
+    startY = int(bbox_predictions[1] * shape)
+    endX = int(bbox_predictions[2] * shape)
+    endY = int(bbox_predictions[3] * shape)
+
+    # Draw the bounding box on the image
+    cv2.rectangle(image_resized, (startX, startY), (endX, endY), (0, 255, 0), 2)
+
+    # Save the image with the bounding box
+    cv2.imwrite("./static/uploads/prediction.jpeg", image_resized)
+
+    return render_template('display.html', filename="prediction.jpeg")
 # ===============================================================================================
 if __name__ == '__main__':
     app.run(debug=True)
